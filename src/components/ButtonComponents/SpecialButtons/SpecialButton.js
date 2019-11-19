42e24f9180f1878ed01a8db1e0ffdd554d72297a
steven oakley <stevenoakley211@live.com>
2019-11-19 17:12:22 -0600

    MVP

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = props => {
   return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    <button className='btn specials'>
+      {props.special/* Display a button element rendering the data being passed down from the parent container on props */}
+    </button>
   );
 };
+export default SpecialButton;
