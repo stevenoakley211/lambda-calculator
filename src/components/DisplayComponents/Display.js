c0f64a167b3b9bb7e37d805c6c8998661bb6e89b
steven oakley <stevenoakley211@live.com>
2019-11-19 19:49:26 -0600

    :Display update

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Display = props => {
-  return <div className="display">{props.number/* Display any props data here */}</div>;
+  return <div className="display">{props.number}</div>;
 };
 
 export default Display;
